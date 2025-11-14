fa8b0669d884f41770c6162f5b5bd8b65ebe2ccb
Kiverly <129836490+Kiverly@users.noreply.github.com>
2025-11-14 18:56:52 +0800

    game.js

--- game.js
+++ game.js
@@ -18,7 +18,7 @@ class FlappyBird {
             width: 34,
             height: 24,
             velocity: 0,
-            gravity: 0.5,
+            gravity: 0.3,
             jump: -8,
             rotation: 0
         };
